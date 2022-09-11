678e3726980fae87cf2fc9f9cc8ed1ac2b1fee4d
Chris Mumford <cmumford@google.com>
2022-09-11 04:15:39 -0700

    Cleanup app code.

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import neopixel
 import board, digitalio
 import feathers3
 
-pins_to_toggle = [
+pins_ids_to_toggle = [
     board.D5,
     board.D6,
     board.D9,
@@ -19,55 +19,68 @@ pins = []
 
 
 def InitPins():
-  global pins, pins_to_toggle
-  for pin_num in pins_to_toggle:
+  global pins, pins_ids_to_toggle
+  for pin_num in pins_ids_to_toggle:
     pin = digitalio.DigitalInOut(pin_num)
     pin.direction = digitalio.Direction.OUTPUT
     pins.append(pin)
 
 
+def InitNeoPixel():
+  """Initialize the NeoPixel."""
+  global pixel
+
+  # Brightness of 0.3 is ample for the 1515 sized LED
+  pixel = neopixel.NeoPixel(board.NEOPIXEL,
+                            1,
+                            brightness=0.3,
+                            auto_write=True,
+                            pixel_order=neopixel.GRB)
+
+  # Turn on the power to the NeoPixel
+  feathers3.set_ldo2_power(True)
+
+
+def PrintBoardInfo():
+  # Say hello
+  print("\nHello from FeatherS3!")
+  print("------------------\n")
+
+  # Show available memory
+  print("Memory Info - gc.mem_free()")
+  print("---------------------------")
+  print("{} Bytes\n".format(gc.mem_free()))
+
+  flash = os.statvfs('/')
+  flash_size = flash[0] * flash[2]
+  flash_free = flash[0] * flash[3]
+  # Show flash size
+  print("Flash - os.statvfs('/')")
+  print("---------------------------")
+  print("Size: {} Bytes\nFree: {} Bytes\n".format(flash_size, flash_free))
+
+  print("Pixel Time!\n")
+
+
+def Initialize():
+  PrintBoardInfo()
+  InitPins()
+  InitNeoPixel()
+
+
 def ToggleLEDs():
+  global pins
+
   feathers3.led_blink()
   for pin in pins:
     pin.value = False if pin.value else True
 
 
-# Create a NeoPixel instance
-# Brightness of 0.3 is ample for the 1515 sized LED
-pixel = neopixel.NeoPixel(board.NEOPIXEL,
-                          1,
-                          brightness=0.3,
-                          auto_write=True,
-                          pixel_order=neopixel.GRB)
+Initialize()
 
-# Say hello
-print("\nHello from FeatherS3!")
-print("------------------\n")
-
-# Show available memory
-print("Memory Info - gc.mem_free()")
-print("---------------------------")
-print("{} Bytes\n".format(gc.mem_free()))
-
-flash = os.statvfs('/')
-flash_size = flash[0] * flash[2]
-flash_free = flash[0] * flash[3]
-# Show flash size
-print("Flash - os.statvfs('/')")
-print("---------------------------")
-print("Size: {} Bytes\nFree: {} Bytes\n".format(flash_size, flash_free))
-
-print("Pixel Time!\n")
-
-# Create a colour wheel index int
+# Rainbow colors on the NeoPixel.
+# Create a color wheel index int.
 color_index = 0
-
-# Turn on the power to the NeoPixel
-feathers3.set_ldo2_power(True)
-
-InitPins()
-
-# Rainbow colors on the NeoPixel
 while True:
   # Get the R,G,B values of the next color
   r, g, b = feathers3.rgb_color_wheel(color_index)
